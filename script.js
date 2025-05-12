af5d85d486ec5bc0f3492340ee613283b3e6b3ae
delfinx <simone.orlando.007@gmail.com>
2025-05-12 21:38:28 +0200

    Esercizio 2 completato

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
 function controllaNumero(num) {
     console.log(num); 
   
-    if (num % 2 === 0) {
+    if(num % 2 === 0) {
       console.log("Numero pari: " + num);
     } 
     else {
       console.log("Numero dispari: " + num);
     }
   
-    if (num > 100) {
+    if(num > 100) {
       alert("Grande!");
     }
   }
   
   
   controllaNumero(101);
+
+    let i = 10;
+
+    while(i >= 0) {
+    console.log(i);
+
+    if(i === 0) {
+        alert("BOOM!");
+    }
+
+        i--; 
+    }
+
   
